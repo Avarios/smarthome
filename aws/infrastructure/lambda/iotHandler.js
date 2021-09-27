c0d20c5f8c6aa744673ab105b949e0fa109ee466
Pawel Warmuth <warmuth.pawel@gmail.com>
2021-09-27 20:07:00 +0200

    Added AWS Infrastructure and lambda iot handler

---
+++ aws/infrastructure/lambda/iotHandler.js
@@ -0,0 +1,49 @@
+// asset-input/lambda/iotHandler.js
+var AWS = require("aws-sdk");
+var bucket = process.env.BUCKET_NAME;
+var handler = async (event, context, callback) => {
+  console.log(event);
+  let fileId = `${makeid(8)}.json`;
+  let s3 = new AWS.S3({
+    apiVersion: "latest"
+  });
+  try {
+    await s3.putObject({
+      Bucket: bucket,
+      Key: fileId,
+      Body: JSON.parse(event.body),
+      ContentType: "application/json; charset=utf-8"
+    }).promise();
+    let response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        fileName: fileId,
+        message: ""
+      }) ,
+      isBase64Encoded: false
+    };
+    callback(null, response);
+  } catch (e) {
+    let error = {
+      statusCode: 500,
+      body: {
+        success: false,
+        fileName: fileId,
+        message: e
+      },
+      isBase64Encoded: false
+    };
+    callback(error);
+  }
+};
+function makeid(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+exports.handler = handler;
