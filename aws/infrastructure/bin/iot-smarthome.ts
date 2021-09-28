995844737ac3af147b96a60eb0042f6dfab19346
Pawel Warmuth <pwarmuth@amazon.de>
2021-09-28 09:53:22 +0200

    changed name of the stack

--- aws/infrastructure/bin/iot-smarthome.ts
+++ aws/infrastructure/bin/iot-smarthome.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { IotAwsBridgeStack } from '../lib/iot-aws-bridge-stack';
+import { IotAwsBridgeStack } from '../lib/iot-smarthome-stack';
 
 const app = new cdk.App();
-new IotAwsBridgeStack(app, 'IotAwsBridgeStack', {
+new IotAwsBridgeStack(app, 'IotSmartHomeStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
